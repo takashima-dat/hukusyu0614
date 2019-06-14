992711cc36e4ab776509d082e5835263f9a1ce42
ozeki <hhdfgg@users.noreply.github.com>
2019-06-14 16:39:40 +0900

    授業終了後

--- hukusyu0614/Form1.Designer.cs
+++ hukusyu0614/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -76,11 +78,33 @@
             this.label3.Text = "label3";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(478, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(325, 97);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "よけとる";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1270, 547);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -97,6 +121,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- hukusyu0614/Form1.cs
+++ hukusyu0614/Form1.cs
@@ -12,29 +12,60 @@ namespace hukusyu0614
 {
     public partial class Form1 : Form
     {
+        //label1用
         int[] vx = new int[3];
         int[] vy = new int[3];
         
-        const int su = 30;
-        const int suu = 20;
-        int[] vxs = new int[su];
-        int[] vys = new int[su];
+        //labelsとenemyの数
+        const int su = 10;
+        const int suu = 5;
+        int[] vxl = new int[su];
+        int[] vyl = new int[su];
+        int[] vxe = new int[suu];
+        int[] vye = new int[suu];
         Label[] labels = new Label[su];
         Label[] enemy = new Label[suu];
 
+        //labelsとenemyの動く速さ
         int rvx = -10;
         int rvy = 11;
 
-        int scp = 100;
-        const int hpm = 10;
+        //スコアとHPの計算
+        int scp = 100;//1labelsのスコア加算量
+        const int hpm = 10;//1enemyあたりのHP減算量
         int sc = 0;
-        int hp = suu * hpm;
+        int hp = suu * hpm;//初期HP = enemyの数 * HP減算量
+        int co = 0;
+
+        int hosi;
+
+        int getCount;
 
         private static Random ra = new Random();
+
+        enum SCENE
+        {
+            TITLE,
+            GAME,
+            GAMEOVER,
+            CLEAR,
+            NONE
+        }
+        /// <summary>
+        /// 現在のシーン
+        /// </summary>
+        SCENE nowScene;
+
+        /// <summary>
+        /// 切り替えたいシーン
+        /// </summary>
+        SCENE nextScene;
 
         public Form1()
         {
             InitializeComponent();
+            nextScene=SCENE.TITLE;
+            nowScene = SCENE.NONE;
 
             for (int i = 0; i < su; i++)
             {
@@ -47,8 +78,8 @@ namespace hukusyu0614
                 labels[i].ForeColor = Color.Orange;
                 labels[i].Left = ra.Next(ClientSize.Width - labels[i].Width);
                 labels[i].Top = ra.Next(ClientSize.Height - labels[i].Height);
-                vxs[i] = ra.Next(rvx, rvy);
-                vys[i] = ra.Next(rvx, rvy);
+                vxl[i] = ra.Next(rvx, rvy);
+                vyl[i] = ra.Next(rvx, rvy);
             }
 
             for (int i = 0; i < suu; i++)
@@ -62,8 +93,8 @@ namespace hukusyu0614
                 enemy[i].ForeColor = Color.Black;
                 enemy[i].Left = ra.Next(ClientSize.Width - enemy[i].Width);
                 enemy[i].Top = ra.Next(ClientSize.Height - enemy[i].Height);
-                vxs[i] = ra.Next(rvx, rvy);
-                vys[i] = ra.Next(rvx, rvy);
+                vxe[i] = ra.Next(rvx, rvy);
+                vye[i] = ra.Next(rvx, rvy);
 
             }
 
@@ -86,60 +117,160 @@ namespace hukusyu0614
 
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        void initProc()
         {
-            label1.Left += vx[0];
-            label1.Top += vy[0];
+            // nextSceneがNONEだったら、何もしない
+            if (nextScene == SCENE.NONE) return;
 
+            nowScene = nextScene;
+            nextScene = SCENE.NONE;
 
-            label2.Text = "SCORE:" + sc;
-            label3.Text = "HP:" + hp;
+            switch(nowScene)
+            {
+                case SCENE.TITLE:
+                    label4.Visible = true;
+                    button1.Visible = true;
+                    for (int i = 0; i < su; i++)
+                    {
+                        labels[i].Visible = false;
+                    }
+                    for (int i = 0; i < suu; i++)
+                    {
+                        enemy[i].Visible = false;
+                    }
+                    label2.Visible = false;
+                    label3.Visible = false;
+                    break;
+                case SCENE.GAME:
+                    label4.Visible = false;
+                    button1.Visible = false;
+                    for (int i = 0; i < su; i++)
+                    {
+                        labels[i].Visible = true;
+                    }
+                    for (int i = 0; i < suu; i++)
+                    {
+                        enemy[i].Visible = true;
+                    }
+                    label2.Visible = true;
+                    label3.Visible = true;
+                    getCount = co;
+                    break;
+                case SCENE.CLEAR:
+                    //ゲームクリアで最終スコア表示
+                        int SC = sc * hp;
+                        MessageBox.Show("GAME CLEAR  SCORE:" + SC);
+                    break;
+            }
+        }
 
+        void updateProc()
+        {
+            if (nowScene==SCENE.GAME)
+            {
+                updateGame();
+            }
+        }
+
+        void updateGame()
+        {
+            //labelsの跳ね返り
             for (int i = 0; i < su; i++)
             {
-                labels[i].Left += vxs[i];
-                labels[i].Top += vys[i];
+                labels[i].Left += vxl[i];
+                labels[i].Top += vyl[i];
 
                 if (labels[i].Left <= 0)
                 {
-                    vxs[i] = Math.Abs(vxs[i]);
+                    vxl[i] = Math.Abs(vxl[i]);
                 }
                 if (labels[i].Top <= 0)
                 {
-                    vys[i] = Math.Abs(vys[i]);
+                    vyl[i] = Math.Abs(vyl[i]);
                 }
                 if (labels[i].Right >= ClientSize.Width)
                 {
-                    vxs[i] = -Math.Abs(vxs[i]);
+                    vxl[i] = -Math.Abs(vxl[i]);
                 }
                 if (labels[i].Bottom >= ClientSize.Height)
                 {
-                    vys[i] = -Math.Abs(vys[i]);
+                    vyl[i] = -Math.Abs(vyl[i]);
                 }
             }
 
+            //enemyの跳ね返り
             for (int i = 0; i < suu; i++)
             {
-                enemy[i].Left += vxs[i];
-                enemy[i].Top += vys[i];
+                enemy[i].Left += vxe[i];
+                enemy[i].Top += vye[i];
 
                 if (enemy[i].Left <= 0)
                 {
-                    vxs[i] = Math.Abs(vxs[i]);
+                    vxe[i] = Math.Abs(vxe[i]);
                 }
                 if (enemy[i].Top <= 0)
                 {
-                    vys[i] = Math.Abs(vys[i]);
+                    vye[i] = Math.Abs(vye[i]);
                 }
                 if (enemy[i].Right >= ClientSize.Width)
                 {
-                    vxs[i] = -Math.Abs(vxs[i]);
+                    vxe[i] = -Math.Abs(vxe[i]);
                 }
                 if (enemy[i].Bottom >= ClientSize.Height)
                 {
-                    vys[i] = -Math.Abs(vys[i]);
+                    vye[i] = -Math.Abs(vye[i]);
                 }
             }
+
+            //マウスのポイントによる動き
+            Point p = PointToClient(MousePosition);
+
+            //labelsをとる、消える・スコア加算
+            for (int i = 0; i < su; i++)
+            {
+                if (labels[i].Left <= p.X &&
+                    labels[i].Right >= p.X &&
+                    labels[i].Top <= p.Y &&
+                    labels[i].Bottom >= p.Y &&
+                    labels[i].Visible==true)
+                {
+                    labels[i].Visible = false;
+                    sc = sc + scp;
+                    hosi++;
+                }
+            }
+
+            //enemyをとる、消える・HP減算
+            for (int i = 0; i < suu; i++)
+            {
+                if (enemy[i].Left <= p.X &&
+                    enemy[i].Right >= p.X &&
+                    enemy[i].Top <= p.Y &&
+                    enemy[i].Bottom >= p.Y&&
+                    enemy[i].Visible == true)
+                {
+                    enemy[i].Visible = false;
+                    hp = hp - hpm;
+                }
+            }
+
+            if (hosi == su)
+                nextScene = SCENE.CLEAR;
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            initProc();
+            updateProc();
+
+            label1.Left += vx[0];
+            label1.Top += vy[0];
+
+            label1.Text = "" + hosi;
+            label2.Text = "SCORE:" + sc;
+            label3.Text = "HP:" + hp;
 
             if (label1.Left <= 0)
             {
@@ -167,47 +298,14 @@ namespace hukusyu0614
             */
             //自力で何も見ないでここまで
 
-            Point p = PointToClient(MousePosition);
-
             /*
+            //timerが止まる
             if (label1.Left <= p.X &&
                     label1.Right >= p.X &&
                     label1.Top <= p.Y &&
                     label1.Bottom >= p.Y)
             {
                 timer1.Enabled = false;
-            }*/
-
-            for (int i = 0; i < su; i++)
-            {
-                if (labels[i].Left <= p.X &&
-                    labels[i].Right >= p.X &&
-                    labels[i].Top <= p.Y &&
-                    labels[i].Bottom >= p.Y)
-                {
-                    labels[i].Visible = false;
-                    sc = sc + scp;
-                }
-            }
-
-            for (int i = 0; i < suu; i++)
-            {
-                if (enemy[i].Left <= p.X &&
-                    enemy[i].Right >= p.X &&
-                    enemy[i].Top <= p.Y &&
-                    enemy[i].Bottom >= p.Y)
-                {
-                    enemy[i].Visible = false;
-                    hp = hp - hpm;
-                }
-            }
-
-            
-            //if (/*黄色い星を全部取ったら*/)
-            /*
-            {
-                int SC=sc*hp;
-                MessageBox.Show("GAME CLEAR  SCORE:" + SC);
             }*/
         }
 
@@ -219,6 +317,11 @@ namespace hukusyu0614
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nextScene = SCENE.GAME;
         }
     }
 }
